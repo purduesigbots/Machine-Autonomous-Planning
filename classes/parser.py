a0c5cacb9e13a5c88b572989dd3fcfd21c966ea7
Micah Rassi <micahrassi@gmail.com>
2022-02-22 14:42:28 -0500

    Add a parser file

---
+++ classes/parser.py
@@ -0,0 +1,10 @@
+
+class Parser:
+
+    # TODO: write code to import an auton script
+    def import_script():
+        pass
+
+    # Export path as cpp script
+    def export_script():
+        pass
