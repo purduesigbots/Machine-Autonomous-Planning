cf772d605dc8b32786da3d16309b33dfaab4a39b
Mihir Laud <mihirlaud@gmail.com>
2022-02-24 20:49:07 -0500

    Allow for stringing movements

--- classes/screen.py
+++ classes/screen.py
@@ -1,4 +1,5 @@
 # import statements
+from classes.movement import Movement
 import tkinter as tk
 
 # constants
@@ -13,11 +14,21 @@ class Window:
         self.root = root
         self.canvas = canvas
         self.temp_line = None
+        self.movements = []
 
+        # bind keyboard input to key_handler callback
+        self.root.bind("<Key>", self.key_handler)
         # bind mouse button input to click_handler callback
         self.root.bind("<Button>", self.click_handler)
         # bind mouse motion input to motion_handler callback
         self.root.bind("<Motion>", self.motion_handler)
+
+    def key_handler(self, event):
+        if self.creating_movement and event.keysym == "Escape":
+            self.creating_movement = False
+            self.canvas.delete(self.temp_line)
+            self.start_point = (0, 0)
+            self.end_point = (0, 0)
 
     # mouse click input handler
     def click_handler(self, event):
@@ -36,12 +47,16 @@ class Window:
             # if not first click
             else:
                 # set creating movement to false and store end point
-                self.creating_movement = False
                 self.end_point = (event.x, event.y)
 
                 # create line between start and end point
-                self.canvas.create_line(self.start_point[0], self.start_point[1], self.end_point[0], self.end_point[1], 
+                line_ref = self.canvas.create_line(self.start_point[0], self.start_point[1], self.end_point[0], self.end_point[1], 
                                      fill="lime", width=5, arrow=tk.LAST, arrowshape=(8, 10, 8))
+                
+                m = Movement(self.start_point, self.end_point, line_ref)
+                self.movements.append(m)
+                
+                self.start_point = self.end_point
         # if mouse click is not on field
         else:
             print("Outside field")
