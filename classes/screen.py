298437a872f0d65911b536f736b2af5086edec71
Mihir Laud <mihirlaud@gmail.com>
2022-02-24 20:34:53 -0500

    Encapsulate code better

--- classes/screen.py
+++ classes/screen.py
@@ -1,91 +1,60 @@
-from turtle import window_height
-import pygame as pg
+# import statements
+import tkinter as tk
 
-width = 1000
-height = 600
+# constants
+SCREEN_HEIGHT = 600
 
-class Screen:
-    pg.init()
+class Window:
 
-    def __init__(self, dark=False):
-        self.movements = []
-        self.sidebar_start = 0
-        self.window = pg.display.set_mode((width,height))
-        self.width = width
-        self.height = height
-        self.field_width = self.height
-        self.dark = dark
-        self.movement_settings_display = None
-        self.lines = (50, 50, 50) if dark else (180, 180, 180)
-        self.bg = (15, 15, 15) if dark else (235, 235, 235)
-        self.tc = (70, 70, 70) if dark else (180, 180, 180)
-        self.update(50)
+    def __init__(self, root, canvas):
+        self.creating_movement = False
+        self.start_point = (0, 0)
+        self.end_point = (0, 0)
+        self.root = root
+        self.canvas = canvas
+        self.temp_line = None
 
-    def draw_settings(self):
-        if self.movement_settings_display == None:
-            return
-        self.movements[self.movement_settings_display].show_settings(
-            self.window, (self.field_width/6, self.height/6))
+        # bind mouse button input to click_handler callback
+        self.root.bind("<Button>", self.click_handler)
+        # bind mouse motion input to motion_handler callback
+        self.root.bind("<Motion>", self.motion_handler)
 
-    def draw_background(self):
-        self.window.fill(self.bg)
-        bg_image = pg.image.load('assets/field.png')
-        bg_image = pg.transform.scale(bg_image, (self.height, self.height))
-        self.window.blit(bg_image, bg_image.get_rect())
-        split_height = self.height/6
-        for i in range(6):
-            sh = i*split_height
-            pg.draw.line(self.window, self.lines, (sh, 0), (sh, self.height))
-            pg.draw.line(self.window, self.lines, (0, sh), (self.height, sh))
-        pg.draw.line(self.window, self.lines, (self.height, 0),
-                     (self.height, self.height), 3)
+    # mouse click input handler
+    def click_handler(self, event):
+        # if mouse click is on field
+        if event.x < SCREEN_HEIGHT:
+            # convert to inches on field
+            # bottom left of field is (0, 0)
+            field_x = event.x / SCREEN_HEIGHT * 24.0 * 6
+            field_y = (600 - event.y) / SCREEN_HEIGHT * 24.0 * 6
 
-    def update(self, dt):
-        self.draw_background()
+            # if this is first click
+            if not self.creating_movement:
+                # set creating movement to true and store starting point
+                self.creating_movement = True
+                self.start_point = (event.x, event.y)
+            # if not first click
+            else:
+                # set creating movement to false and store end point
+                self.creating_movement = False
+                self.end_point = (event.x, event.y)
 
-        l = len(self.movements)
-        for m in range(l):
-            self.movements[m].draw_arrow(self.window)
-        for m in range(l if l < 12 else 12):
-            self.movements[m+self.sidebar_start].draw_sidebar(
-                self.window, (self.field_width, m*self.height/12))
-        self.draw_settings()
-        pg.time.delay(dt)
-        pg.display.update()
+                # create line between start and end point
+                self.canvas.create_line(self.start_point[0], self.start_point[1], self.end_point[0], self.end_point[1], 
+                                     fill="lime", width=5, arrow=tk.LAST, arrowshape=(8, 10, 8))
+        # if mouse click is not on field
+        else:
+            print("Outside field")
 
-    def move_sidebar(self, direction):
-        self.sidebar_start += direction
-        self.sidebar_start = 0 if self.sidebar_start < 0 else self.sidebar_start
-        self.sidebar_start = len(
-            self.movements)-12 if self.sidebar_start > len(self.movements)-12 else self.sidebar_start
+    # mouse motion inpute handler
+    def motion_handler(self, event):
+        # if currently drawing a line
+        if self.creating_movement:
+            # delete the current temp line
+            self.canvas.delete(self.temp_line)
 
-    def check_clicks(self, pos):
-        sidebar_clicks = []
-        for m in self.movements:
-            if m.toggle_settings(pos):
-                sidebar_clicks.append(int(m.name.split(" ")[1])-1)
-            m.update(color=(50, 200, 50))
-        clicked = max(sidebar_clicks) if len(sidebar_clicks) > 0 else None
-        if not clicked == None:
-            self.movement_settings_display = clicked if not self.movement_settings_display == clicked else None
-        if not self.movement_settings_display == None:
-            self.movements[self.movement_settings_display].update(
-                color=(35, 100, 35))
-            self.movements[self.movement_settings_display].show_settings(
-                self.window, (self.field_width/6, self.height/6))
+            # create a new temp line between start point and current mouse position
+            self.temp_line = self.canvas.create_line(self.start_point[0], self.start_point[1], event.x, event.y, 
+                                               fill="lime", width=5, arrow=tk.LAST, arrowshape=(8, 10, 8))
 
-    def add_move(self, m):
-        m.update(bg=self.bg, tc=self.tc)
-        self.movements.append(m)
 
-    def edit_move(self, new_move, i=None):
-        i = len(self.movements)-1 if not i else i
-        new_move.update(bg=self.bg, tc=self.tc)
-        self.movements[i] = new_move
-
-    def remove_move(self, i=None):
-        i = len(self.movements)-1 if not i else i
-        del self.movements[i]
-
-    def reset(self):
-        self.__init__(size=(self.width, self.height), dark=self.dark)
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # import statements
 import tkinter as tk
+from classes.screen import Window
 from PIL import Image, ImageTk
 
 # constants
@@ -24,59 +25,7 @@ can = tk.Canvas(root, width=SCREEN_HEIGHT,
 can.place(anchor=tk.NW, x=0, y=0)
 can.create_image(0, 0, anchor=tk.NW, image=field)
 
-class Window:
-    def __init__(self, root, canvas):
-        self.creating_movement = False
-        self.start_point = (0, 0)
-        self.end_point = (0, 0)
-        self.root = root
-        self.canvas = canvas
-        self.temp_line = None
-
-w = Window(root, can)
-
-# mouse click input handler
-def click_handler(event):
-    # if mouse click is on field
-    if event.x < SCREEN_HEIGHT:
-        # convert to inches on field
-        # bottom left of field is (0, 0)
-        field_x = event.x / SCREEN_HEIGHT * 24.0 * 6
-        field_y = (600 - event.y) / SCREEN_HEIGHT * 24.0 * 6
-
-        # if this is first click
-        if not w.creating_movement:
-            # set creating movement to true and store starting point
-            w.creating_movement = True
-            w.start_point = (event.x, event.y)
-        # if not first click
-        else:
-            # set creating movement to false and store end point
-            w.creating_movement = False
-            w.end_point = (event.x, event.y)
-
-            # create line between start and end point
-            w.canvas.create_line(w.start_point[0], w.start_point[1], w.end_point[0], w.end_point[1], 
-                                 fill="lime", width=5, arrow=tk.LAST, arrowshape=(8, 10, 8))
-    # if mouse click is not on field
-    else:
-        print("Outside field")
-
-# mouse motion inpute handler
-def motion_handler(event):
-    # if currently drawing a line
-    if w.creating_movement:
-        # delete the current temp line
-        w.canvas.delete(w.temp_line)
-
-        # create a new temp line between start point and current mouse position
-        w.temp_line = w.canvas.create_line(w.start_point[0], w.start_point[1], event.x, event.y, 
-                                           fill="lime", width=5, arrow=tk.LAST, arrowshape=(8, 10, 8))
-
-# bind mouse button input to click_handler callback
-w.root.bind("<Button>", click_handler)
-# bind mouse motion input to motion_handler callback
-w.root.bind("<Motion>", motion_handler)
+Window(root, can)
 
 # start main loop
-w.root.mainloop()
+root.mainloop()
